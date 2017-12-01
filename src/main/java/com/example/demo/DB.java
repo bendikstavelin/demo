397a70a7e017b781f5a36a25651ca6b7b65f431b
Bendik Stavelin <bendik.stavelin@acando.no>
2017-12-01 10:10:48 +0100

    first commit

---
+++ src/main/java/com/example/demo/DB.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DB {
+
+}
+
+@Component
+interface PersonDB extends MongoRepository<Person, String> {
+
+}
